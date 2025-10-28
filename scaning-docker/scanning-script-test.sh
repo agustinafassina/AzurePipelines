59e8d9a829ee7f68153fbece5a26ff99815dde0f
Agustina Fassina | ENG <agustinafassina@gmail.com>
2025-10-27 21:01:15 -0300

    variable rename

--- scaning-docker/scanning-script-test.sh
+++ scaning-docker/scanning-script-test.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
-DOCKER_REPOSITORYNAME=api-test
+docker_repositoryname=api-test
 sudo apt-get install -y wget gnupg2
 sudo apt install jq -y
 wget https://github.com/aquasecurity/trivy/releases/download/v0.67.0/trivy_0.67.0_Linux-64bit.deb
 sudo dpkg -i trivy_0.67.0_Linux-64bit.deb
 
-echo "Docker image '$DOCKER_REPOSITORYNAME' built successfully."
+echo "Docker image '$docker_repositoryname' built successfully."
 
 # Run vulnerability scan with Trivy
 echo "Starting vulnerability scan on the image..."
-trivy image --severity CRITICAL,HIGH,MEDIUM,LOW --exit-code 1 "$DOCKER_REPOSITORYNAME"
+trivy image --severity CRITICAL,HIGH,MEDIUM,LOW --exit-code 1 "$docker_repositoryname"
 
 # Check scan result
 if [ $? -ne 0 ]; then
-echo "Vulnerabilities detected in image '$DOCKER_REPOSITORYNAME'."
+echo "Vulnerabilities detected in image '$docker_repositoryname'."
 exit 1
 else
 echo "No critical, high, or medium vulnerabilities found."
